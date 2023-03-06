0a4ce35d10366bd8ddc1891c5efa8c379e145a7f
Sergei Driga <drigaserge@gmail.com>
2023-03-06 22:30:48 +0300

    initial

---
+++ src/Bicycle.java
@@ -0,0 +1,30 @@
+public class Bicycle {
+// был нарушен принцип инкапсуляции. Добавил гетттеры и сеттеры, сделал поля класса private
+    private String modelName;
+    private int wheelsCount;
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1 () {
+        System.out.println("\nTask 1");
+    }
+
+    public static void task2 () {
+        System.out.println("\nTask 2");
+    }
+
+    public static void task3 () {
+        System.out.println("\nTask 3");
+    }
+
+    public static void task4 () {
+        System.out.println("\nTask 4");
+    }
+
+    public static void task5 () {
+        System.out.println("\nTask 5");
+    }
+
+}
